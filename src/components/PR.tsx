7395738a0ea553b035ddcf185ca71ce6396baba1
HaudinFlorence <haudin.florence@gmail.com>
2024-03-27 09:42:47 +0100

    Fix bug of failing app when the markdown input is empty. Introduce major updates to be able to render properly the different nodes of the tree. Make the inline math rendering work. Fix failure that was present when the tiping the url of the PR directive, when it was not complete yet. Update markdownRender and DefaultComponent. Place the mardownEditor and the markdownRenderer in 2 differents files.

--- src/components/PR.tsx
+++ src/components/PR.tsx
@@ -9,14 +9,24 @@ type PRdata = {
 };
 
 function getPRInfosfromURL(URL: string): PRdata {
-  const pieces1 = URL.split("https://github.com/");
-  const pieces2 = pieces1[1].split("/");
-  const owner = pieces2[0];
-  const repo = pieces2[1];
-  const PRNumber = Number(pieces2[3]);
+  const githubBaseUrl = "https://github.com/";
+  const minLength = githubBaseUrl.length;
+  if (!URL) {
+    return { owner: "", repo: "", PRNumber: null };
+  } else {
+    if (!URL.includes(githubBaseUrl) || URL.length < minLength) {
+      return { owner: "", repo: "", PRNumber: null };
+    } else {
+      const pieces1 = URL.split(githubBaseUrl);
+      const pieces2 = pieces1[1].split("/");
+      const owner = pieces2[0];
+      const repo = pieces2[1];
+      const PRNumber = Number(pieces2[3]);
 
-  const PRdata: PRdata = { owner: owner, repo: repo, PRNumber: PRNumber };
-  return PRdata;
+      const PRdata: PRdata = { owner: owner, repo: repo, PRNumber: PRNumber };
+      return PRdata;
+    }
+  }
 }
 
 interface PRLinkProps {
@@ -67,43 +77,47 @@ function PullRequestComponent(props: PRComponentProps): JSX.Element {
 
   const searchPR = async (pullRequestURL: string) => {
     let imageURL = "";
-    const PRInfos = getPRInfosfromURL(pullRequestURL);
-    const response = await request("GET /repos/{owner}/{repo}/pulls", {
-      owner: PRInfos.owner,
-      repo: PRInfos.repo,
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    });
+    if (pullRequestURL) {
+      const PRInfos = getPRInfosfromURL(pullRequestURL);
+      if (PRInfos.owner && PRInfos.repo) {
+        const response = await request("GET /repos/{owner}/{repo}/pulls", {
+          owner: PRInfos.owner,
+          repo: PRInfos.repo,
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        });
 
-    response.data.forEach((data, index) => {
-      if (data.number === PRInfos.PRNumber) {
-        const sha = data.head.sha;
-        imageURL =
-          "https://opengraph.githubassets.com/" +
-          sha +
-          "/" +
-          PRInfos.owner +
-          "/" +
-          PRInfos.repo +
-          "/pull/" +
-          PRInfos.PRNumber;
-        const title =
-          data.title +
-          " by " +
-          data.user.login +
-          " . Pull Request #" +
-          data.number +
-          " . " +
-          PRInfos.owner +
-          "/" +
-          PRInfos.repo +
-          " - Github";
-        setpullRequestURLImage(imageURL);
-        setpullRequestDescription(data.body);
-        setpullRequestSummary(title);
+        response.data.forEach((data, index) => {
+          if (data.number === PRInfos.PRNumber) {
+            const sha = data.head.sha;
+            imageURL =
+              "https://opengraph.githubassets.com/" +
+              sha +
+              "/" +
+              PRInfos.owner +
+              "/" +
+              PRInfos.repo +
+              "/pull/" +
+              PRInfos.PRNumber;
+            const title =
+              data.title +
+              " by " +
+              data.user.login +
+              " . Pull Request #" +
+              data.number +
+              " . " +
+              PRInfos.owner +
+              "/" +
+              PRInfos.repo +
+              " - Github";
+            setpullRequestURLImage(imageURL);
+            setpullRequestDescription(data.body);
+            setpullRequestSummary(title);
+          }
+        });
       }
-    });
+    }
   };
 
   useEffect(() => {
---
+++ src/components/markdownEditor.tsx
@@ -0,0 +1,73 @@
+import { MarkdownRenderer } from "./markdownRenderer";
+import { defaultOptions, mystParse } from "myst-parser";
+import { defaultDirectives } from "myst-directives";
+import { helloDirective } from "../directives/helloDirective";
+import { pullRequestDirective } from "../directives/PRDirective";
+import { VFile } from "vfile";
+import { GenericNode, GenericParent } from "myst-common";
+import { useState } from "react";
+
+import {
+  captionParagraphTransform,
+  liftMystDirectivesAndRolesTransform,
+  mathTransform,
+  mystTargetsTransform,
+} from "myst-transforms";
+
+const allDirectives = defaultDirectives.concat(
+  helloDirective,
+  pullRequestDirective
+);
+
+const customOptions = {
+  ...defaultOptions,
+  directives: allDirectives,
+};
+
+function transformTree(tree: GenericParent) {
+  const vfile = new VFile(); // used for logging error messages
+  tree.children.forEach((child) => {
+    if (child.name === "figure") {
+      liftMystDirectivesAndRolesTransform(tree);
+      mystTargetsTransform(tree);
+    }
+    if (child.name === "math" || child.type === "math") {
+      mathTransform(tree, vfile, { macros: {} });
+    }
+    if (child.name === "code") {
+      captionParagraphTransform(tree);
+    }
+  });
+}
+
+export function MarkdownEditor() {
+  const initial = "test";
+  const [postContent, setPostContent] = useState(initial);
+  let tree: GenericParent;
+  if (postContent.length === 0) {
+    const emptyNode: GenericNode = { type: "paragraph", value: "" };
+    tree = { type: "paragraph", children: [emptyNode] };
+  } else {
+    tree = mystParse(postContent, customOptions);
+  }
+  transformTree(tree);
+  return (
+    <>
+      <label>
+        <h1 className="app-title"> Myst react app</h1>
+        <p className="text-instruction-class">Enter some markdown:</p>
+        <textarea
+          className="textarea-class"
+          value={postContent}
+          onChange={(e) => {
+            setPostContent(e.target.value);
+            /*if (e.target.value.length === 0) {
+              setPostContent("");
+            }*/
+          }}
+        />
+      </label>
+      <MarkdownRenderer tree={tree}></MarkdownRenderer>
+    </>
+  );
+}
